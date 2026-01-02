9821bd3943838d3aead7842649a3a856135a73d4
太田　宏昇 <ootahir0417@gmail.com>
2026-01-02 13:32:40 +0900

    first commit.

--- components.py
+++ components.py
@@ -79,28 +79,29 @@ def display_initial_ai_message():
             "こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。"
             "左のサイドメニューで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。"
         )
-
-def display_initial_ai_message():
-    """
-    AIメッセージの初期表示
-    """
-    with st.chat_message("assistant"):
+##############################################################
+#2026年1月㏡2日修正
+##############################################################
+## タイトル表示"""
+    #AIメッセージの初期表示
+    #"""
+    # with st.chat_message("assistant"):
         # 「st.success()」とすると緑枠で表示される
-        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        #st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
 
         # 「社内文書検索」の機能説明
-        st.sidebar.markdown("**【「社内文書検索」を選択した場合】**")
+        #st.sidebar.markdown("**【「社内文書検索」を選択した場合】**")
         # 「st.info()」を使うと青枠で表示される
-        st.sidebar.info("入力内容と関連性が高い社内文書のありかを検索できます。")
+        #st.sidebar.info("入力内容と関連性が高い社内文書のありかを検索できます。")
         # 「st.code()」を使うとコードブロックの装飾で表示される
         # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-        st.sidebar.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
+        #st.sidebar.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
 
         # 「社内問い合わせ」の機能説明
-        st.sidebar.markdown("**【「社内問い合わせ」を選択した場合】**")
-        st.sidebar.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-        st.sidebar.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
-
+        #st.sidebar.markdown("**【「社内問い合わせ」を選択した場合】**")
+        #st.sidebar.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
+        #st.sidebar.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
+###############################################################
 
 def display_conversation_log():
     """
