7fb44958cc94aa3331c29139b285d7ffc037c5da
太田　宏昇 <ootahir0417@gmail.com>
2026-01-03 20:40:58 +0900

    first commit.

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ display.apply_sidebar_gray()
 display.apply_ai_comment_background()
 #タイトル表示
 display.display_app_title()
-# サイド
+# サイドバー
 display.display_select_mode()
 display.display_sidebar_description()
 
